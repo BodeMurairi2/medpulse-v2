d3d7d24568fb023977737f6a9e27ede1484bc5e8
BodeMurairi2 <b.murairi@alustudent.com>
2025-10-30 14:13:11 +0200

    Pydantic model for department table

---
+++ api/schemas/hospital.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, EmailStr
+
+class HospitalCreate(BaseModel):
+    hospital_name: str
+    hospital_email: EmailStr
+    hospital_country: str
+    hospital_city: str
+    hospital_license: str
+    password: str
